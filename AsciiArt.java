cab75d928608fcfea4141b6bf4736d67442daac3
cnkehoe <50990317+cnkehoe@users.noreply.github.com>
2019-05-24 06:16:05 -0600

    Add files via upload

---
+++ AsciiArt.java
@@ -0,0 +1,31 @@
+/**
+ * @version date (in_ISO_8601 format: 2019-01-17
+ * @author Carly Kehoe
+ */
+
+public class AsciiArt {
+    
+    /**
+     * Prints my name in ASCII art to the console
+     * @param args Command-line arguments are ignored.
+     */
+    
+    public static void main(String[] args) {
+        System.out.println
+                ("  _____           _       \n" +
+                " / ____|         | |      \n" +
+                "| |     __ _ _ __| |_   _ \n" +
+                "| |    / _` | '__| | | | |\n" +
+                "| |___| (_| | |  | | |_| |\n" +
+                " \\_____\\__,_|_|  |_|\\__, |\n" +
+                "                     __/ |\n" +
+                "                    |___/");
+    }
+    
+   /*
+     link to ascii art generator i used because i can't ascii art
+     https://www.ascii-art-generator.org/
+    */
+   
+    
+}
